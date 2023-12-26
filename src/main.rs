eed2fb959ea81e69f08e21e9898bbb8233ffee4e
Magnus Küderli <magnusku@gmx.de>
2023-12-27 00:13:12 +0100

    added more functionality like deleting, printing, clearing etc

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,63 @@
 use std::collections::HashMap;
 
 fn main() {
+    let mut database = Database::new().expect("!!ERROR!!: Database creation failed");
+    
     let mut arguments = std::env::args().skip(1);
     
-    let key = arguments.next().expect("no key found");
-    let value = arguments.next().expect("no value found");
-    
-    println!("The key is '{}' and the value is '{}'!", key, value);
+    let action = arguments.next().expect("!!ERROR!!: No action given").to_uppercase();
 
-    let mut database = Database::new().expect("Database::new() panicked");
-    database.insert(key.to_uppercase(), value.clone());
-    database.insert(key, value);
-    database.flush();
+    if action=="W" {
+        if std::env::args().len() > 4 { println!("!WARNING!: too many args given"); }
+
+        let key = arguments.next().expect("!!ERROR!!:  No valid key given").to_uppercase();
+        let value = arguments.next().expect("!!ERROR!!:  No valid value given");
+        
+        if database.map.contains_key(&key) {println!("!WARNING!: Key was overwritten")}
+        database.insert(key.clone(), value.clone());
+        
+        println!("Set value '{}' for the key '{}'!", &value, &key);
+    }
+
+    else if action=="R" {
+        if std::env::args().len() > 3 { println!("!WARNING!: too many args given"); }
+
+        let key = arguments.next().expect("!!ERROR!!:  No valid key given").to_uppercase();
+        println!("'{}' contains the value '{}'",&key, database.map.get(&key).expect("!!ERROR!!: Key not valid"));
+    }
+
+    else if action=="P" {
+        if std::env::args().len() > 2 { println!("!WARNING!: too many args given"); }
+        
+        let mut comment = "no entries\n"; 
+        if database.raw.len() != 0 { comment = ""}
+
+        println!("\n=====================\n{}{}=====================\n", comment, &database.raw);
+    }
+
+    else if action=="D" { 
+        if std::env::args().len() > 3 { println!("!WARNING!: too many args given"); }
+
+        let key = arguments.next().expect("!!ERROR!!:  No valid key given").to_uppercase();
+        database.map.remove(&key).expect("!!ERROR!!: Key not valid");
+        println!("Successfully removed {} from database", key);
+    }
+
+    else if action=="E" {
+        if std::env::args().len() > 1 { println!("!WARNING!: too many args given"); }
+        database.clear();
+        println!("Database cleared");
+    }
+
+    else {
+        println!("!!ERROR!!: no valid option given")
+    }
+
 }
 
 struct Database {
     map: HashMap<String, String>,
+    raw: String,
     flushed: bool
 }
 
@@ -28,8 +70,8 @@ impl Database {
         let contents = std::fs::read_to_string("kv.db")?;
         for line in contents.lines() {
             let mut chunks = line.splitn(2, '\t');
-            let key = chunks.next().expect("faulty key!");
-            let value = chunks.next().expect("faulty value!");
+            let key = chunks.next().expect("!!ERROR!!: database corrupted - faulty key!");
+            let value = chunks.next().expect("!!ERROR!!: database corrupted - faulty value!");
             map.insert(key.to_owned(), value.to_owned());
         }
         // parse string
@@ -37,6 +79,7 @@ impl Database {
         Ok(
             Database{
                 map: map,
+                raw: contents,
                 flushed: false
             }
         )
@@ -46,7 +89,11 @@ impl Database {
         self.map.insert(key, value);
     }
 
-    fn flush(&mut self) {
+    fn clear(&mut self) {
+        let _ = self.map.drain();
+    }
+
+    fn _flush(&mut self) {
         do_flush(self);
     }
 }
@@ -68,7 +115,7 @@ fn do_flush(db: &mut Database) {
             contents.push('\n');
         }
         
-        std::fs::write("kv.db", contents).expect("failed to write");
+        std::fs::write("kv.db", contents).expect("!!ERROR!!: failed to write");
         
         db.flushed = true;
         println!("Data written to disk");
